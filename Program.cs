786505ea621bb730c82e824f0735b119eaf2706c
jmgallen <35116410+jmgallen@users.noreply.github.com>
2022-09-23 15:28:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace Abstraction
+{
+    //  Abstract class
+    abstract class Hero
+    {
+        // Abstract method
+        public abstract void attack();
+
+        // Regular method
+        public void defend()
+        {
+            Console.WriteLine("Hero defends.");
+        }
+    }
+
+    // Derived class (inherits from Hero)
+    class Druid : Hero
+    {
+        // Override method
+        public override void attack()
+        {
+            // Body of attack
+            Console.WriteLine("Druid shifts into animal and attacks.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Druid myDruid = new Druid();
+            myDruid.attack();
+            myDruid.defend();
+        }
+    }
+}
